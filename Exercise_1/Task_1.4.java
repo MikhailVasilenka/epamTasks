78a8e0345805411098f8e357c370ba0406f09a28
Mikhail <44245866+MikhailVasilenka@users.noreply.github.com>
2018-10-24 23:50:23 +0300

    Update Task_1.4.java

--- Exercise_1/Task_1.4.java
+++ Exercise_1/Task_1.4.java
@@ -10,31 +10,14 @@ public class Main {
         System.out.println("Enter four digit number: ");
         String s1 = in.nextLine();
 
-            if (s1.charAt(0) < s1.charAt(1) && s1.charAt(1) < s1.charAt(2) && s1.charAt(2) < s1.charAt(3)){
-                        System.out.println("Increasing!");
+        if (s1.charAt(0) < s1.charAt(1) && s1.charAt(1) < s1.charAt(2) && s1.charAt(2) < s1.charAt(3)) {
+            System.out.println("Increasing!");
+        } else {
+            if (s1.charAt(0) > s1.charAt(1) && s1.charAt(1) > s1.charAt(2) && s1.charAt(2) > s1.charAt(3)) {
+                System.out.println("Decreasing!");
             }
-
-            if (s1.charAt(0) > s1.charAt(1) && s1.charAt(1) > s1.charAt(2) && s1.charAt(2) > s1.charAt(3)){
-                        System.out.println("Decreasing!");
+            else {
+                System.out.println( "This is not a sequence!");
             }
-
-                    if(s1.charAt(0) == s1.charAt(1) || s1.charAt(1) == s1.charAt(2) || s1.charAt(2) == s1.charAt(3)){
-                        System.out.println("Your number " + s1 + " don't work with our condition!");
-                    }
-                    else if( s1.charAt(0) > s1.charAt(1) && s1.charAt(1) < s1.charAt(2) && s1.charAt(2) > s1.charAt(3)){
-                        System.out.println("Your number " + s1 + " don't work with our condition!");
-                    }
-                    else if (s1.charAt(0) < s1.charAt(1) && s1.charAt(1) > s1.charAt(2) && s1.charAt(2) < s1.charAt(3)){
-                        System.out.println("Your number " + s1 + " don't work with our condition!");
-                    }
-                    else if (s1.charAt(0) < s1.charAt(1) && s1.charAt(1) < s1.charAt(2) && s1.charAt(2) > s1.charAt(3)){
-                        System.out.println("Your number " + s1 + " don't work with our condition!");
-                    }
-                    else {
-                            System.out.println("Your number " + s1 + " don't work with our condition!");
-                    }
-
         }
-
     }
-
